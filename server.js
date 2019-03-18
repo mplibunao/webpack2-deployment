2d0c1e346d4c4ec06d13fd97b847f0bd3e87ca86
mplibunao <markpaololibunao@gmail.com>
2019-03-18 11:00:35 +0800

    Serves the bundled assets for production

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 const express = require('express');
-const webpackMiddleware = require('webpack-dev-middleware');
-const webpack = require('webpack');
-const webpackConfig =  require('./webpack.config.js')
+const path = require('path');
 
 const app = express();
 
-app.use(webpackMiddleware(webpack(webpackConfig)));
+// Server routes...
+// Make sure to put the server routes above the webpack stuff
+// or else you will never hit these routes
+app.get('/hello', (req, res) => res.send({ hi: 'there' }))
 
-app.listen(3050, () => console.log('Listening'));
+if (process.env.NODE_ENV !== 'production') {
+  const webpackMiddleware = require('webpack-dev-middleware');
+  const webpack = require('webpack');
+  const webpackConfig =  require('./webpack.config.js')
+  app.use(webpackMiddleware(webpack(webpackConfig)));
+} else {
+  // Opens up dist folder
+  app.use(express.static('dist'));
+
+  // For browser history (react-router)
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+  });
+}
+
+app.listen(process.env.PORT || 3050, () => console.log('Listening'));
